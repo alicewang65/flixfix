9a880730690eddc76b498df5d514de39eaaf6b0b
Alice Wang <alice.wang428@gmail.com>
2021-06-20 15:11:17 -0400

    fix space issue in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ class Header extends React.Component {
                 <h1 className="header">FLiX FiX</h1>
                 <p className="bodytext">
                     Everyone enjoys a good movie! There are a tremendous amount of movies and it can 
-                    be difficult to find detailed information about each one. FLiX FiX is <em>the</em>  
-                    tool to help you get your movie fix.
+                    be difficult to find detailed information about each one. FLiX FiX is <em>the</em> tool 
+                    to help you get your movie fix.
                 </p>
                 <p className="bodytext">
                     <span className="highlighttext"> Enter any movie title or keyword in the title into 
